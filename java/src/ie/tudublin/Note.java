a4e08ddc72dde6b59931222d49b174177edddbfb
Jina Park <jinapark119@gmail.com>
2021-04-22 10:00:38 +0100

    bla

--- java/src/ie/tudublin/Note.java
+++ java/src/ie/tudublin/Note.java
@@ -4,21 +4,89 @@ public class Note
 {
     private char note;
     private int duration;
-    public char getNote() {
-        return note;
-    }
-    public void setNote(char note) {
-        this.note = note;
-    }
-    public int getDuration() {
-        return duration;
-    }
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
+    
+
     @Override
-    public String toString() {
-        return "Note [duration=" + duration + ", note=" + note + "]";
+    public String toString()
+    {
+        return note + "\t" + duration + "\t" + ((duration == 2) ? "Crotchet" : "Quaver");
     }
     
+
+    public Note(char note, int duration)
+    {
+        this.note = note;
+        this.duration = duration;
+    }
+    
+    public char getNote()
+    {
+        return note;
+    }
+
+    public void setNote(char note)
+    {
+        this.note = note;
+    }
+
+    public int getDuration()
+    {
+        return duration;
+    }
+
+    public void setDuration(int duration)
+    {
+        this.duration = duration;
+    }
+
+    private String scale = "DEFGABcd";
+
+    private int noteValue()
+    {
+        for(int i = 0; i < scale.length(); i++)
+        {
+            if(note == scale.charAt(i))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void render(ScoreDisplay sd, float x, boolean highlight)
+    {
+        float y = sd.bottom - (noteValue() * (sd.gap * 0.5f));
+        sd.noStroke();
+        if(highlight)
+        {
+            sd.fill(255, 0, 0);
+        }
+        else
+        {
+            sd.fill(0);
+        }
+        sd.ellipse(x, y, 18, 18);
+        sd.strokeWeight(2);
+
+        if(highlight)
+        {
+            sd.stroke(255, 0, 0);
+            sd.fill(255, 0, 0);
+        }
+        else
+        {
+            sd.stroke(0);
+            sd.fill(0);
+        }
+
+        sd.line(x + 8, y, x + 8, y - 50);
+
+        if(duration == 1)
+        {
+            sd.line(x + 8, y - 50, x + 20, y - 30);
+        }
+        sd.fill(0);
+        sd.textSize(16);
+        sd.text(note, x, sd.top - 20);
+    }
 }
--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -8,16 +8,59 @@ import processing.core.PApplet;
 
 public class ScoreDisplay extends PApplet
 {
-	String score = "DEFGABcd";
+	//String score = "DEFGABcd";
 	//String score = "D2E2F2G2A2B2c2d2";
-	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
+	String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
 	
 	ArrayList<Note> notes = new ArrayList<Note>();
 
+
+	private void printNotes()
+	{
+		for(Note n:notes)
+		{
+			System.out.println(n);
+		}
+	}
+
 	private void parseNotes()
 	{
-		
+		int i = 0;
+		while(i < score.length())
+		{
+			char c = score.charAt(i);
+			int duration = 1;
+			if(i < score.length() - 1) // At least one character to read further
+			{
+				char next = score.charAt(i + 1);
+				if(Character.isDigit(next))
+				{
+					duration = next - '0';
+					i++;
+				}
+			}
+			i++;
+			Note n = new Note(c, duration);
+			notes.add(n);
+		}
 	}
+
+	float border;
+	float top = 100;
+	float gap = 20;
+	float bottom = top + (gap * 4) + (gap * 0.5f);
+
+
+	public void drawStaveLines()
+	{
+		stroke(0);
+		for(int i = 0; i < 5; i++)
+		{
+			float y = top + (i * gap);
+			line(border, y, width - border, y);
+		}
+	}
+
 
 	public void settings()
 	{
@@ -31,16 +74,26 @@ public class ScoreDisplay extends PApplet
 
 	public void setup() 
 	{
-		
+		parseNotes();
+		printNotes();
+
+		border = width * 0.1f;
 	}
 
 	public void draw()
 	{
 		background(255);
-		
+		drawStaveLines();
+		drawNotes();
 	}
 
 	void drawNotes()
 	{
+		for(int i = 0; i < notes.size(); i++)
+		{
+			float x = border + (i * gap * 2) + 50;
+			Note n = notes.get(i);
+			n.render(this, x, (mouseX > x - 20 && mouseX < x + 20));
+		}
 	}
 }
